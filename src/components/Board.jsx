9b698fd9e577946a2f9454dac3aa6bb210e10514
faresch4 <84391262+faresch4@users.noreply.github.com>
2024-01-07 16:57:31 -0600

    Refined the refactored code a bit

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 import Tile from "./Tile";
 
 function Board({
-  handleCLick,
+  handleClick,
   board,
   winner,
   handleMouseEnter,
@@ -11,136 +11,36 @@ function Board({
   calculateWinner,
 }) {
   function getClassName(index) {
-    let className = ''
-    if(![2,5,8].includes(index)) className += 'right-border ' 
-    if(index < 6) className += 'bottom-border'
-    return className
+    let className = "";
+    if (![2, 5, 8].includes(index)) className += "right-border ";
+    if (index < 6) className += "bottom-border";
+    return className.trim();
   }
-  console.log()
+  console.log(getClassName(1));
+  const commonProps = {
+    handleMouseEnter,
+    handleMouseLeave,
+    isX,
+    unMount,
+    winner,
+  };
+
   return (
     <div className="board">
-      <Tile
-        className={getClassName(0)}
-        doWhenClick={() => {
-          handleCLick(0);
-        }}
-        value={board[0]}
-        handleMouseEnter={() => handleMouseEnter(0)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(0)}
-        unMount={unMount}
-        isX={isX}
-        winner={winner}
-/>
-      <Tile
-        className={getClassName(1)}
-        doWhenClick={() => {
-          handleCLick(1);
-        }}
-        value={board[1]}
-        handleMouseEnter={() => handleMouseEnter(1)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(1)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="bottom-border"
-        doWhenClick={() => {
-          handleCLick(2);
-        }}
-        value={board[2]}
-        handleMouseEnter={() => handleMouseEnter(2)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(2)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="bottom-border right-border"
-        doWhenClick={() => {
-          handleCLick(3);
-        }}
-        value={board[3]}
-        handleMouseEnter={() => handleMouseEnter(3)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(3)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="bottom-border right-border"
-        doWhenClick={() => {
-          handleCLick(4);
-        }}
-        value={board[4]}
-        handleMouseEnter={() => handleMouseEnter(4)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(4)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="bottom-border"
-        doWhenClick={() => {
-          handleCLick(5);
-        }}
-        value={board[5]}
-        handleMouseEnter={() => handleMouseEnter(5)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(5)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="right-border"
-        doWhenClick={() => {
-          handleCLick(6);
-        }}
-        value={board[6]}
-        handleMouseEnter={() => handleMouseEnter(6)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(6)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className="right-border"
-        doWhenClick={() => {
-          handleCLick(7);
-        }}
-        value={board[7]}
-        handleMouseEnter={() => handleMouseEnter(7)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(7)}
-        winner={winner}
-        unMount={unMount}
-        isX={isX}
-      />
-      <Tile
-        className=""
-        doWhenClick={() => {
-          handleCLick(8);
-        }}
-        winner={winner}
-        value={board[8]}
-        handleMouseEnter={() => handleMouseEnter(8)}
-        handleMouseLeave={() => handleMouseLeave()}
-        handleCLick={() => handleCLick(8)}
-        unMount={unMount}
-        isX={isX}
-      />
-      {winner ? `${winner} is the winner` : ""}
-
-      {board.every((b) => b != null) && !winner ? <span>No winner</span> : ""}
-    </div>
+      {board.map((value, index) =>  (
+         <Tile 
+         key={index}
+         className={getClassName(index)}
+         doWhenClick={() => handleClick(index)}
+         value={value}
+         {...commonProps}
+         />
+      ))}
+         {winner ? `${winner} is the winner` : ""}
+         {board.every((b) => b != null) && !winner ? <span>No winner</span> : ""}
+     </div>
   );
 }
 
 export default Board;
+
