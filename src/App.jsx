a72c734011668f7687ad3806a9748529fe72aff7
faresch4 <84391262+faresch4@users.noreply.github.com>
2024-06-09 19:05:35 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from  'C:/Users/DELL/Desktop/Portfolio/vite-project/src/components/Header.jsx'
+function App() {
+    
+
+  return (
+    <>
+     <Header /> 
+    </>
+  )
+}
+
+export default App
